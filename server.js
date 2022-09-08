502100dea4f4b846b8b056bc41c1848fa1110a40
Beth-Hollinger <hollingere15@gmail.com>
2022-09-07 22:12:10 -0400

    add controllers and test functionality

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ const app = express();
 require("dotenv").config();
 const mongoose = require("mongoose");
 const Product = require("./models/products")
-
 const Seed = require("./models/seedroute")
-
-const methodOverride = require("method-override")
+const methodOverride = require("method-override");
+const productRouter = require("./controllers/productRouter");
 
 //DATABASE CONFIGURATION
 mongoose.connect(process.env.DATABASE_URL, {
@@ -24,28 +23,30 @@ db.on('disconnected', () => console.log('mongo disconnected'));
 // Body parser middleware: give us access to req.body
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"))
+app.use('/products', productRouter)
+app.use(express.static('public'))
 
 //ROUTES
-app.get('/products/seedroute', (req, res) => {
-    Product.deleteMany({}, (error, allProducts) => { });
-    Product.create(Seed, (error, data) => {
-        res.redirect('/products');
-    })
-})
+// app.get('/products/seedroute', (req, res) => {
+//     Product.deleteMany({}, (error, allProducts) => { });
+//     Product.create(Seed, (error, data) => {
+//         res.redirect('/products');
+//     })
+// })
 
-//INDEX
-app.get('/products', (req, res) => {
-    Product.find({}, (error, allProducts) => {
-        res.render('index.ejs', {
-            products: allProducts,
-        });
-    });
-});
+// //INDEX
+// app.get('/products', (req, res) => {
+//     Product.find({}, (error, allProducts) => {
+//         res.render('index.ejs', {
+//             products: allProducts,
+//         });
+//     });
+// });
 
-//NEW
-app.get('/products/new', (req, res) => {
-    res.render('new.ejs');
-});
+// //NEW
+// app.get('/products/new', (req, res) => {
+//     res.render('new.ejs');
+// });
 
 //DELETE
 
@@ -55,53 +56,53 @@ app.get('/products/new', (req, res) => {
 //   })
 
 
-app.delete("/products/:id", (req, res) => {
-    Product.findByIdAndDelete(req.params.id, (err, data) => {
-        res.redirect("/products")
-    })
-})
+// app.delete("/products/:id", (req, res) => {
+//     Product.findByIdAndDelete(req.params.id, (err, data) => {
+//         res.redirect("/products")
+//     })
+// })
 
-//UPDATE
-app.put("/products/:id/buy", (req, res) => {
-    Product.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        {new: true},
-        (error, updatedProduct) => {
-            updatedProduct.qty -= 1
-            console.log(updatedProduct)
-            updatedProduct.save()
-            res.redirect(`/products/${req.params.id}`)
-        }
-    )
-})
+// //UPDATE
+// app.put("/products/:id/buy", (req, res) => {
+//     Product.findByIdAndUpdate(
+//         req.params.id,
+//         req.body,
+//         {new: true},
+//         (error, updatedProduct) => {
+//             updatedProduct.qty -= 1
+//             console.log(updatedProduct)
+//             updatedProduct.save()
+//             res.redirect(`/products/${req.params.id}`)
+//         }
+//     )
+// })
 
-//CREATE
-app.post('/products', (req, res) => {
-    Product.create(req.body, (error, createdProduct) => {
-        res.redirect('/products');
-    });
-})
+// //CREATE
+// app.post('/products', (req, res) => {
+//     Product.create(req.body, (error, createdProduct) => {
+//         res.redirect('/products');
+//     });
+// })
 
 
 
-//SHOW
-app.get('/products/:id', (req, res) => {
-    Product.findById(req.params.id, (err, foundProduct) => {
-        res.render('show.ejs', {
-            product: foundProduct
-        });
-    });
-});
-//EDIT
+// //SHOW
+// app.get('/products/:id', (req, res) => {
+//     Product.findById(req.params.id, (err, foundProduct) => {
+//         res.render('show.ejs', {
+//             product: foundProduct
+//         });
+//     });
+// });
+// //EDIT
 
-app.get("/products/:id/edit", (req, res) => {
-    Product.findById(req.params.id, (error, foundProduct) => {
-        res.render("edit.ejs", {
-            product: foundProduct,
-        })
-    })
-})
+// app.get("/products/:id/edit", (req, res) => {
+//     Product.findById(req.params.id, (error, foundProduct) => {
+//         res.render("edit.ejs", {
+//             product: foundProduct,
+//         })
+//     })
+// })
 
 // LISTENER
 const PORT = process.env.PORT;
